702fb82ee4a95846791e36d3f9c032dc65ee0f8e
iaevelis <31777481+iaevelis@users.noreply.github.com>
2017-09-08 13:47:23 -0700

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,29 @@
+RECIPE 1
+	down
+	down
+down
+down
+down
+down
+down
+down
+	right
+right
+RECIPE 2
+	up
+	up
+	up
+	up
+	up
+	up
+	up
+	up
+	right
+	right
+RECIPE 1
+RECIPE 2
+RECIPE 1
+RECIPE 2
+RECIPE 1
+right	
+up
