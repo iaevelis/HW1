ee5f37bc1a515d39957537d1056a55f383a3fe6a
iaevelis <31777481+iaevelis@users.noreply.github.com>
2017-09-08 13:50:33 -0700

    Update puzzle5.js

--- puzzle5.js
+++ puzzle5.js
@@ -1,12 +1,12 @@
 RECIPE 1
-right
-right
-if get color is red
-	up
-if get color is blue	
-down
-right
-right
+	right
+	right
+	if get color is red
+		up
+	if get color is blue	
+		down
+	right
+	right
 ---------------
 RECIPE 1
 RECIPE 1
