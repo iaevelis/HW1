fe42527130d5d475ff31f535675c135c5d101034
iaevelis <31777481+iaevelis@users.noreply.github.com>
2017-09-13 10:29:57 -0700

    Update puzzle4.js

--- puzzle4.js
+++ puzzle4.js
@@ -1,19 +1,17 @@
-down
-down
-down
-down
-right
-get color
-save
-down
-set color
-right
-right
-up
-right
-get color
-save
-down
-set color
-right
-right
+down();
+down()
+down()
+down()
+right()
+var firstColor=getColor();
+down()
+setColor(firstColor);
+right()
+right()
+up()
+right()
+var secondColor=getColor();
+down()
+setColor(secondColor);
+right()
+right()
