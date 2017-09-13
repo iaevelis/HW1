d1763a33d188b8e738508d9064736f5e70f598b8
iaevelis <31777481+iaevelis@users.noreply.github.com>
2017-09-13 09:58:48 -0700

    Update puzzle7.js

--- puzzle7.js
+++ puzzle7.js
@@ -1,20 +1,27 @@
-RECIPE 1
-  Down 
-  Repeat if get color isn’t red
-    down
-  if get color is red 	
-    right
-    right
-RECIPE 2
-  up 
-  Repeat if get color isn’t red
-    up
-  if get color is red 
-    right
-    right
----------------
-RECIPE 1
-RECIPE 2
-RECIPE 1
-RECIPE 2
-RECIPE 1
+function heis(){
+  down()
+  while(getColor()!="red") {
+  	down()
+	}
+	if (getColor()=="red"){
+    right()
+    right()
+	}
+}
+
+function duo(){
+  up()
+  while(getColor()!="red") {
+  	up()
+	}
+	if (getColor()=="red"){
+    right()
+    right()
+	}
+}
+
+heis()
+duo()
+heis()
+duo()
+heis()
